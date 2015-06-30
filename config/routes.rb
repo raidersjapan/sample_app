8ced18c471eb8d8b4e3dfad728d1a8e1f21d0471
Takayuki Shinozaki <raidersjapan@gmail.com>
2015-06-30 03:49:08 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ SampleApp::Application.routes.draw do
   }
   resources :users, only: [:show]
   root  'static_pages#home'
-  
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
