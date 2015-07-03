074e9c96b9b18610a07d3cb499ab2e90a52eeccb
Takayuki Shinozaki <raidersjapan@gmail.com>
2015-07-03 13:12:36 +0000

    Change Sign in button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  
+  before_filter :authenticate_user!, :except=>[:show]
+
+  def index
+    @users = User.all    
+  end
+
   def show
     @user = User.find(params[:id])
   end
