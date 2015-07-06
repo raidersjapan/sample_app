1b0ddefadc1114f9db2781a77d6671802aa28ed8
Takayuki Shinozaki <raidersjapan@gmail.com>
2015-07-06 11:55:50 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  validates :name, presence: true, length: { maximum: 50 }  
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  def feed
+    # このコードは準備段階です。
+    # 完全な実装は、次回以降に行います。
+    Micropost.where("user_id = ?", id)
+  end  
 end
