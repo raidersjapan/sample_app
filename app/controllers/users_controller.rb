51a49e7d1529956b090449f302ad36cff6e49981
Takayuki Shinozaki <raidersjapan@gmail.com>
2015-07-06 04:09:21 +0000

    Finish user destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :edit, :update]
+  before_filter :authenticate_user!, :except=>[:show]
+  before_action :admin_user,     only: :destroy
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
 
   def index
-    @users = User.paginate(page: params[:page])    
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,5 +25,11 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end  
+  end
+
+  private
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
