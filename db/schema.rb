1b6e3e1237dc0c759ef1aedf7b54be71ee1736ac
Takayuki Shinozaki <raidersjapan@gmail.com>
2015-07-31 10:20:46 +0000

    Make a kadai1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730032444) do
+ActiveRecord::Schema.define(version: 20150731060211) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150730032444) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "age"
   end
 
   create_table "microposts", force: true do |t|
